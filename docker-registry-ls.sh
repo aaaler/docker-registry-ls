1ce211e74bf64de64b747a42e3a807948cc4e3a6
root <root@web388.local>
2016-02-25 07:31:17 +0300

    add paginator workaround

--- docker-registry-ls.sh
+++ docker-registry-ls.sh
@@ -4,12 +4,12 @@ if [ "$#" -ne "1" ]; then
   exit 1
 fi
 
-curl -sf https://$1/v2/_catalog | jq  -r '.["repositories"] | .[]' > repo_list
+curl -sf https://$1/v2/_catalog?n=10000 | jq  -r '.["repositories"] | .[]' > repo_list
 
 while read REPO
 do
   echo -n >tag_list
-  curl -sf https://$1/v2/$REPO/tags/list | jq -r '.["tags"] | .[]' > tag_list
+  curl -sf https://$1/v2/$REPO/tags/list?n=10000 | jq -r '.["tags"] | .[]' > tag_list
   while read TAG
   do
     echo ${REPO}:${TAG}
