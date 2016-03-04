fbe7047c1c819ad26ab9b1967d339db5b56ec1f6
Alexey Griazin <aaaler@gmail.com>
2016-03-04 19:45:32 +0300

    v1 support added

--- docker-registry-ls.sh
+++ docker-registry-ls.sh
@@ -4,16 +4,19 @@ if [ "$#" -ne "1" ]; then
   exit 1
 fi
 
-curl -sf https://$1/v2/_catalog?n=10000 | jq  -r '.["repositories"] | .[]' > repo_list
-
+curl -sf https://$1/v1/search?q= | jq  -r '.["results"] | .[] | .name' > repo_list
+curl -sf https://$1/v2/_catalog?n=10000 | jq  -r '.["repositories"] | .[]' >> repo_list
 while read REPO
 do
   echo -n >tag_list
-  curl -sf https://$1/v2/$REPO/tags/list?n=10000 | jq -r '.["tags"] | .[]' > tag_list
+  curl -sf https://$1//v1/repositories/$REPO/tags | jq -r 'keys | .[]' > tag_list
+  curl -sf https://$1/v2/$REPO/tags/list?n=10000 | jq -r '.["tags"] | .[]' >> tag_list
+
   while read TAG
   do
     echo ${REPO}:${TAG}
   done < tag_list
 done < repo_list
+
 
 
